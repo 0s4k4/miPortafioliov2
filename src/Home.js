13aa19d801c0da573198552d39d33ae8053b0c99
soulboss@gmail.com <soulboss@gmail.com>
2024-05-11 11:15:51 -0500

    cambios de incio

---
+++ src/Home.js
@@ -0,0 +1,20 @@
+//src /Home.js
+
+import React from "react";
+
+
+import './Home.css';
+
+
+const Home = () => {
+    return (
+      <div className="home">
+        <h1>Mi Nombre</h1>
+        <div className="icon-container">
+          <img src="ruta/al/icono.svg" alt="Icono de persona en computadora" />
+        </div>
+      </div>
+    );
+  }
+
+  export default Home;
